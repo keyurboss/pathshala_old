4b46361f4880679c9cc060da98db39d0e72ec10f
Keyur Shah <keyurshah3939@gmail.com>
2020-05-03 22:35:46 +0530

    Profile Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatTabsModule} from '@angular/material/tabs';
 import { ProfileComponent } from './profile/profile.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { ProfileComponent } from './profile/profile.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatTabsModule
+    MatTabsModule,
+    MatDividerModule,
+    MatListModule,
+    MatCardModule
   ],
   schemas:[CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
-
+  profileDetails={
+    "Gender":"Male",
+    "Mobile No.":"9428393489",
+    "Alternate No.":"7016879936",
+    "D.O.B" : "16/12/1997",
+    "City":"Ahmedabad",
+    "Email ID":"keyurshah3939@gmail.com"
+  };
+  profileDetailsKeys;
+  constructor() { 
+  }
   ngOnInit(): void {
+    this.profileDetailsKeys = Object.keys(this.profileDetails);
   }
 
 }
