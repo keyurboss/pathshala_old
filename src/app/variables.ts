cff3522d540807cebda09811092a491dd265f862
Keyur Shah <keyur@rpsoftech.net>
2020-05-30 02:31:35 +0530

    fine tuning

--- src/app/variables.ts
+++ src/app/variables.ts
@@ -2,34 +2,33 @@ import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
 import Swal from 'sweetalert2';
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class Variables {
-    
-    private baseUrl;
-    constructor(){  
-        let protocol = 'http:'
-        let url = location.hostname;
-        let port = '3030';
-        if (location.protocol === 'https:' && environment.production) {
-            protocol='https:';
-            port='3443';
-        }
-        if(environment.production){
-            url = "server.rpsoftech.xyz";
-        }
-        this.baseUrl = protocol+'//'+url+':'+port;
+  private baseUrl;
+  constructor() {
+    let protocol = 'http:';
+    let url = location.hostname;
+    let port = '3030';
+    if (location.protocol === 'https:' && environment.production) {
+      protocol = 'https:';
+      port = '3443';
     }
-    getBaseUrl():String{
-        return this.baseUrl;
+    if (environment.production) {
+      url = 'server.rpsoftech.xyz';
     }
-    getApiUrl():String{
-        return this.baseUrl+"/api";
-    }
-    getLoginUrl():String{
-        return this.baseUrl+"/loginserver";
-    }
+    this.baseUrl = protocol + '//' + url + ':' + port;
+  }
+  getBaseUrl(): string {
+    return this.baseUrl;
+  }
+  getApiUrl(): string {
+    return this.baseUrl + '/api';
+  }
+  getLoginUrl(): string {
+    return this.baseUrl + '/loginserver';
+  }
 }
 export class GlobalVariables {
-    public static swal = Swal;
+  public static swal = Swal;
 }
